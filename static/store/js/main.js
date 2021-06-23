bacc5ca1a34fceb8c9c7bdedd04c263e8a622883
Your Name <you@example.com>
2021-06-23 15:38:01 +0000

    xx

--- static/store/js/main.js
+++ static/store/js/main.js
@@ -1,1 +1,54 @@
 console.log("static files OK")
+var x = document.getElementById("demo");
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(initMap);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    x.innerHTML =
+        "<a target='_blank' href=https://www.google.com/maps/@" +
+        position.coords.latitude +
+        "," +
+        position.coords.longitude +
+        ",50m/data=!3m1!1e3>gMaps</a><br>"
+    //+ "Lat: " +
+    //position.coords.latitude +
+    //" Long: " +
+    //position.coords.longitude;
+}
+
+// In this example, we center the map, and add a marker, using a LatLng object
+// literal instead of a google.maps.LatLng object. LatLng object literals are
+// a convenient way to add a LatLng coordinate and, in most cases, can be used
+// in place of a google.maps.LatLng object.
+let map;
+
+function initMap(position) {
+  const mapOptions = {
+    zoom: 8,
+    center: { lat: position.coords.latitude, lng: position.coords.longitude },
+  };
+  map = new google.maps.Map(document.getElementById("map"), mapOptions);
+  const marker = new google.maps.Marker({
+    // The below line is equivalent to writing:
+    // position: new google.maps.LatLng(-34.397, 150.644)
+    position: { lat: position.coords.latitude, lng: position.coords.longitude },
+    map: map,
+  });
+  // You can use a LatLng literal in place of a google.maps.LatLng object when
+  // creating the Marker object. Once the Marker object is instantiated, its
+  // position will be available as a google.maps.LatLng object. In this case,
+  // we retrieve the marker's position using the
+  // google.maps.LatLng.getPosition() method.
+  const infowindow = new google.maps.InfoWindow({
+    content: "<p>Marker Location:" + marker.getPosition() + "</p>",
+  });
+  google.maps.event.addListener(marker, "click", () => {
+    infowindow.open(map, marker);
+  });
+}
